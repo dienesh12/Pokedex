597e4e4456933c04256a3ff293d9a13838370e3d
dienesh12 <dineshpathakhere@gmail.com>
2023-08-20 11:13:11 +0530

    added deployment code

--- server/server.js
+++ server/server.js
@@ -29,4 +29,23 @@ app.use("/api/pokeml", express.static(path.join(__dirname, "/mod")));
 app.use('/api/users', require('./Routes/userRoute'))
 app.use('/api/collections', require('./Routes/collectionRoute'))
 
+// ---------------------- Deployment ----------------------
+
+var __dirname2 = path.resolve().split("\\")
+__dirname2.pop()
+const __dirname1 = __dirname2.join("/")
+console.log(__dirname1);
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/client/build")))
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+    })
+} else {
+    app.get("/", (req, res) => {
+      res.send("API is Runnning Successfully!");
+    })
+}
+
+// ---------------------- Deployment ----------------------
+
 app.listen(PORT, () => { console.log(`Server started on PORT ${PORT}`) })
